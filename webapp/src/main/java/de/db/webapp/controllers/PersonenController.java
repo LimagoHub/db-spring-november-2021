f55741d15c703b3d2bff5cbfd6b4c514df15a1b7
Jo Wagner <jowagner@limago.de>
2021-11-08 15:38:27 +0100

    PersonenControllerMitSwagger

--- webapp/src/main/java/de/db/webapp/controllers/PersonenController.java
+++ webapp/src/main/java/de/db/webapp/controllers/PersonenController.java
@@ -2,14 +2,13 @@ package de.db.webapp.controllers;
 
 import de.db.webapp.controllers.dtos.PersonDTO;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.swing.text.html.Option;
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -29,5 +28,43 @@ public class PersonenController {
             optional = Optional.empty();
         }
         return ResponseEntity.of(optional);
+    }
+
+    @ApiResponse(responseCode = "200", description = "Liste wurde erstellt")
+    @ApiResponse(responseCode = "500", description = "Interner Serverfehler")
+    @GetMapping(path = "", produces= {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<List<PersonDTO>> findAll(@RequestParam(defaultValue = "", required = false) String vorname, @RequestParam(defaultValue = "", required = false) String nachname) {
+
+        System.out.println(vorname);
+        System.out.println(nachname);
+        List<PersonDTO> result = List.of(
+                PersonDTO.builder().id("1").vorname("John").nachname("Doe").build(),
+                PersonDTO.builder().id("1").vorname("John").nachname("Wayne").build(),
+                PersonDTO.builder().id("1").vorname("John").nachname("Wick").build(),
+                PersonDTO.builder().id("1").vorname("John").nachname("McClaine").build(),
+                PersonDTO.builder().id("1").vorname("John").nachname("Rambo").build(),
+                PersonDTO.builder().id("1").vorname("John Boy").nachname("Walton").build()
+        );
+
+        return ResponseEntity.ok(result);
+    }
+    @ApiResponse(responseCode = "204", description = "Person wurde geloescht")
+    @ApiResponse(responseCode = "404", description = "Person wurde nicht gefunden")
+    @ApiResponse(responseCode = "500", description = "Interner Serverfehler")
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable  String id){
+        if(id.startsWith("1")){
+           return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @ApiResponse(responseCode = "200", description = "Person wurde geaendert")
+    @ApiResponse(responseCode = "201", description = "Person wurde erfasst")
+    @ApiResponse(responseCode = "500", description = "Interner Serverfehler")
+    @PutMapping(path="", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> saveOrUpdate(@RequestBody  PersonDTO person){
+        System.out.println("Person wurde gespeichert");
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
